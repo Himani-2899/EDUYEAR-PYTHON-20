2e0dbc85b1a4bca2f653a26e3bcac9a63e0a11c5
Himani-2899 <78131172+Himani-2899@users.noreply.github.com>
2021-05-23 00:44:37 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,12 @@
+emp_name  = "Himani"
+emp_id  =  200120
+emp_dob = 28.11
+emp_age = 21
+print(type(emp_name))
+print(type(emp_id))
+print(type(emp_age))
+print(type(emp_dob))
+
+print('One of our Employee name is {}.She is {} years old. Her Employee id is {}'.format(emp_name, emp_age, emp_id))
+
+
