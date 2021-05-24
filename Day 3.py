070edcc2681fd1ee6e056151dcbadad3dc26ec48
Himani-2899 <78131172+Himani-2899@users.noreply.github.com>
2021-05-24 23:52:06 +0530

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,20 @@
+# Age Calculator
+Current_year = int(input("Enter Current Year :"))
+Birth_year = int(input("Enter the Birth Year :"))
+Age = Current_year - Birth_year
+print("Age of person is:",Age )
+
+
+# WAP To make Simple Calculator
+num1 = int(input("Enter first no :"))
+num2 = int(input("Enter second no :"))
+print("Addition of two no is : {}".format (num1+num2))
+print("Substraction of two no is : {}".format (num1-num2))
+print("Multiplication of two no is : {}".format (num1*num2))
+print("Division of two no is : {}".format (num1/num2))
+print("Decimal division of two no is : {}".format (num1//num2))
+print("Remainder division of two no is : {}".format (num1%num2))
+print("num1^num2 is : {}".format (num1**num2))
+
+
+
