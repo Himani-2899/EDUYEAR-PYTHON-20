6f0a13a47743c1dfb8c996fff45de24cc9c3a6df
Himani-2899 <78131172+Himani-2899@users.noreply.github.com>
2021-05-26 19:11:06 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,17 @@
+# 1.Use String Functions to count the occurence of "y" in a word given by user.
+
+word = input("Enter any Word")
+print( "y occured",word.count("y"),"times")
+
+
+# 2. Take input of a String and print it's Even Indexed Characters
+str=input(" Enter any String ")
+print(str[::2])
+
+
+# 3. WAP to Swapcase
+print("Use of SWAPCASE")
+str = " dOn't lEt yEsTeRdAy tAkE uP tOO mUcH oF tOdAy!! "
+print(str.swapcase())
+
+
